83d14dd0622f55cd2fceb198879121759ee1f094
David Pop <david.pop@icloud.com>
2022-02-14 21:04:36 +0100

    Solved pbinfo - copii2

---
+++ pbinfo/copii2/main.cpp
@@ -0,0 +1,12 @@
+#include <cstdio>
+
+int main(){
+
+    int fete, baieti, zile;
+
+    scanf("%d %d %d", &fete, &baieti, &zile);
+
+    printf("%d\n", zile * (fete * 3 + baieti * 2));
+
+    return 0;
+}
