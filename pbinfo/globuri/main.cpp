cbfd72749f62128547de3e74fd8a22bf15530be7
David Pop <david.pop@icloud.com>
2022-02-14 21:00:27 +0100

    Solved pbinfo - globuri

---
+++ pbinfo/globuri/main.cpp
@@ -0,0 +1,15 @@
+#include <cstdio>
+
+int main(){
+
+    int albe, rosii, verzi;
+
+    scanf("%d", &albe);
+
+    rosii = 2 * albe;
+    verzi = rosii - 3;
+
+    printf("%d\n", albe + rosii + verzi);
+
+    return 0;
+}
