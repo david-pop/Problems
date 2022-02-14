e372ae4c1b86e9b47dd90b0d795058435e1a2d69
David Pop <david.pop@icloud.com>
2022-02-14 21:08:24 +0100

    Solved pbinfo - animale

---
+++ pbinfo/animale/main.cpp
@@ -0,0 +1,15 @@
+#include <cstdio>
+
+int main(){
+
+    int caini, pisici, gaini;
+
+    scanf("%d", &caini);
+
+    pisici = caini * 2;
+    gaini = pisici * 2;
+
+    printf("%d\n", caini + pisici + gaini);
+
+    return 0;
+}
